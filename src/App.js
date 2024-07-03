f3f1d0444b1a1671fc24c38213e61e4b689891df
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-03 05:27:28 -0700

    use useReducer Hook to handle data fetched from fake API and when not able to fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import DateCounter from './DateCounter'; 
+import { useEffect, useReducer } from "react";
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+const initialState = {
+  value: [],
+
+  state: "loading..",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "recieved":
+      return { ...state, value: action.payload, state: "loaded" };
+
+    case "dataFailer":
+      return { ...state, state: "failed to fetch" };
+
+    default:
+      return new Error("data not able to fetched ");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "recieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailer" }));
+  }, []);
+
   return (
-    <>
-      <DateCounter />
-    </>
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1 /15</p>
+        <p> Question </p>
+      </Main>
+    </div>
   );
 }
