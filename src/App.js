0c09fd6c41335c0d69b194763f5a09160aee348d
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-03 10:03:30 -0700

    fetch the data from a fake API and apply switch conditions for successfull and failed data fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
 
+const initialstate ={
+
+  questions :[],
+
+  step : 'loading'
+}
+
+function reducer (state , action)
+{
+  switch(action.type){
+    case 'dataRecived':
+      return {...state , questions : action.payload, step : 'Ready'}
+    case 'notRecived':
+      return {...state , step : 'Error'}
+      default:
+        return new Error("data not able to fetched ");  
+}
+}
 export default function App() {
+
+  const [state , dispatch]= useReducer(reducer , initialstate)
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type :'dataRecived', payload : data}))
+      .catch((err) => dispatch({type : 'notRecived'}));
+  }, []);
   return (
-    <div className='app'>
-      <Header/>
+    <div className="app">
+      <Header />
+      <Main>
+        <p> 1/15</p>
+        <p> Questions </p>
+      </Main>
     </div>
-   
   );
 }
