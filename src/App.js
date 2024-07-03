a9aa4e254e6ca6140e114f29e8fc7c67f5cc3670
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-03 05:36:03 -0700

    Revert "pushed the code in wrong project repo"
    This reverts commit f3f1d0444b1a1671fc24c38213e61e4b689891df.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import { useEffect, useReducer } from "react";
-// import DateCounter from "./DateCounter";
-import Header from "./Header";
-import Main from "./Main";
-const initialState = {
-  value: [],
-
-  state: "loading..",
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "recieved":
-      return { ...state, value: action.payload, state: "loaded" };
-
-    case "dataFailer":
-      return { ...state, state: "failed to fetch" };
-
-    default:
-      return new Error("data not able to fetched ");
-  }
-}
-
+import DateCounter from './DateCounter'; 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  useEffect(function () {
-    fetch("http://localhost:8000/questions")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "recieved", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailer" }));
-  }, []);
-
   return (
-    <div className="app">
-      <Header />
-
-      <Main>
-        <p>1 /15</p>
-        <p> Question </p>
-      </Main>
-    </div>
+    <>
+      <DateCounter />
+    </>
   );
 }
